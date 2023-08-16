ba51478aab395da5b2be264eb9b4c65d38d5c592
good-coder-bad-boy <69414897+good-coder-bad-boy@users.noreply.github.com>
2023-08-16 21:33:44 +1000

    adopted async structure

--- config.py
+++ config.py
@@ -1,19 +1,93 @@
 """
-Config file for specifying pins, etc.
+Config file for specifying pins, modes, solutions, etc.
+
+All pin numbers assume BCM, unless MODE is changed.
 """
 
+from RPIO import BCM, BOARD
+
+from typing import NamedTuple
+
+# ---------------------------------------------------------------------------- #
+#                               Classes and util                               #
+#                                                                              #
+#                                DO NOT MODIFY!                                #
+# ---------------------------------------------------------------------------- #
+
+class ServoType(NamedTuple):
+    """
+    An internal class representing a servo motor.
+    """
+
+    pin: int
+    "The control pin number"
+    mindc: int = 1000
+    "The 0deg duty cycle in μs"
+    maxdc: int = 2000
+    "The 180deg duty cycle in μs"
+
+class CombinationType(NamedTuple):
+    """
+    An internal class representing a combination lock.
+    """
+
+    pins: tuple[int, ...]
+    "The pins of the buttons (order sensitive)"
+    seq: list[int]
+    "The correct sequence of button indexes"
+
+# ---------------------------------------------------------------------------- #
+#                             Application settings                             #
+# ---------------------------------------------------------------------------- #
+
 DEBUG = False
+
+# ------------------------------- PWM settings ------------------------------- #
 
 PWM0 = 12
 PWM1 = 13
 
-SERVO0 = 23
-SERVO1 = 24
+# ------------------------------ Servo settings ------------------------------ #
 
-# subject to change
-COMBINATION = [3, 0, 2, 1, 0, 0, 3, 3]
+SERVO0PIN = 23
+"The control pin of the first door servo"
+SERVO1PIN = 24
+"The control pin of the second door servo"
 
-COMBO0 = 17
-COMBO1 = 27
-COMBO2 = 22
-COMBO3 = 10
+# --------------------------- Combination settings --------------------------- #
+
+COMBOPINS = (17, 27, 22, 10)
+"The pins of the buttons (order sensitive)"
+COMBOSEQ = [3, 0, 2, 1, 0, 0, 3, 3]
+"The correct sequence of buttons"
+
+
+# ---------------------------------------------------------------------------- #
+#                                Advanced config                               #
+#                                                                              #
+#                  ONLY TOUCH IF YOU KNOW WHAT YOU ARE DOING!                  #
+# ---------------------------------------------------------------------------- #
+
+MODE = BCM # BCM or BOARD
+"Pin numbering mode"
+
+SERVO0MIN = 1000
+"The 0deg duty cycle in μs"
+SERVO0MAX = 2000
+"The 180deg duty cycle in μs"
+
+SERVO1MIN = 1000
+"The 0deg duty cycle in μs"
+SERVO1MAX = 2000
+"The 180deg duty cycle in μs"
+
+# ---------------------------------------------------------------------------- #
+#                                Initialisation                                #
+#                                                                              #
+#                                DO NOT MODIFY!                                #
+# ---------------------------------------------------------------------------- #
+
+SERVO0 = ServoType(SERVO0PIN, SERVO0MIN, SERVO0MAX)
+SERVO1 = ServoType(SERVO1PIN, SERVO1MIN, SERVO1MAX)
+
+COMBO = CombinationType(COMBOPINS, COMBOSEQ)
--- main.py
+++ main.py
@@ -1,37 +1,98 @@
+#!/usr/bin/python3
+
 """
-The main script
+The application's main script.
 """
 
+from asyncio import gather, run
+from functools import lru_cache
+from logging import error, info
 from platform import machine
+from math import floor
+from RPIO import PWM
 from RPIO import *
 
 from config import *
 
-
-def reset() -> None:
-    setmode(BCM)
+from typing import Coroutine
 
 
-def comlockseq(pins: tuple[int, int, int, int], callback: function) -> None:
+setmode(MODE)
+
+servoctl = PWM.Servo()
+
+setup(SERVO0, OUT)
+setup(SERVO1, OUT)
+
+
+async def reset() -> None:
+    await gather(
+        opendoor(SERVO0, False),
+        opendoor(SERVO1, False)
+    )
+
+
+lru_cache(50)
+async def degtodutycycle(servo: ServoType, deg: int) -> int:
+    """
+    Convert degrees to duty cycle in μs based on a servo's config.
+    """
+
+    assert 0 <= deg <= 180, f"Servo range invalid: Cannot move servo on pin {servo.pin} to {deg}deg."
+
+    dcdelta = servo.maxdc - servo.mindc
+
+    off = dcdelta * deg / 180
+
+    return floor(servo.mindc + off)
+
+async def comlockseq(combo: CombinationType, callback: Coroutine) -> None:
     """
     Implement a 4 digit combintation lock running on gpio pins.
     """
 
-def opendoor(pins: int, dir: bool) -> None:
+    while True:
+        for p in combo.pins:
+            pass # do stuff
+
+        break
+
+    return await callback
+
+async def opendoor(servo: ServoType, deg: int) -> None:
     """
-    Activate a servo running on a gpio pin
+    Activate a servo running on a gpio pin.
     """
 
+    dc = await degtodutycycle(servo, deg)
+
+    servoctl.set_servo(servo.pin, dc)
+
+
+async def main() -> None:
+    """
+    The application's main loop.
+    """
+
+    info("Starting main loop...")
+
+    while True:
+        await reset()
+
+        try:
+            await comlockseq(COMBO, opendoor(SERVO0, True))
+
+        except EOFError:
+            info("^D detected, reseting...")
+            continue
+
+        except KeyboardInterrupt:
+            info("^C detected, exiting")
+            break
 
 
 if __name__ == "__main__":
     if machine() != "armv7l":
         raise EnvironmentError("This script must be run on Raspberry Pi")
 
-    while True:
-        reset()
-
-        try:
-            comlockseq((COMBO0, COMBO1, COMBO2, COMBO3), lambda: opendoor(SERVO0, True))
-        except EOFError:
-            break
+    run(main())
